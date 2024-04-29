263532cdaf8e5f2f3ba69ee9beb43d7f49452a3f
Kendy - CMC Global <thanhnhan.lionus@gmail.com>
2024-04-29 11:18:19 +0700

    setup efcore

--- AuthenApp.WebApi/Contexts/ApplicationDbContext.cs
+++ AuthenApp.WebApi/Contexts/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+public partial class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
     }
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
 }
